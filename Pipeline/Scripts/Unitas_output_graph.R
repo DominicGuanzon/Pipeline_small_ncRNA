783091a4fc57d2a20a89b8a9209da56bae0b605c
Dominic Guanzon <Dominic.Guanzon@gmail.com>
2022-04-01 17:27:32 +1000

    The column samples names in combined Unitas files are now sorted based on the sample file supplied by the user. Also, modified the way the files are read by specifying column names directly. Graph output modifed to be more useful.

--- Pipeline/Scripts/Unitas_output_graph.R
+++ Pipeline/Scripts/Unitas_output_graph.R
@@ -7,6 +7,10 @@ plot_summary_graphs <- function(data_path, out_path) {
     # Read in data
     Input_file <- read.csv(data_path)
 
+    # Extract total_counts for plotting.
+    Total_reads = Input_file[grep("^RNA_species|^Raw_counts", colnames(Input_file))]
+    Total_reads = Total_reads[Total_reads$RNA_species == "Total_counts", ]
+    
     # Extract percentage columns and extract only rows of interest (major RNA species).
     Percentage_data = Input_file[grep("^RNA_species|^Percentages", colnames(Input_file))]
     rows_to_extract = !grepl(paste0("miRNA:homo_sapiens$|miRNA:other$|genomic_rRNA$|Mt_rRNA$|",
@@ -20,23 +24,35 @@ plot_summary_graphs <- function(data_path, out_path) {
     # Rename
     Percentage_data[grep("mapped to piRNA producing loci", Percentage_data$RNA_species), "RNA_species"] = "piRNA producing loci"
     colnames(Percentage_data) = gsub("^Percentages_", "", colnames(Percentage_data))
+    colnames(Total_reads) = gsub("^Raw_counts_", "", colnames(Total_reads))
 
     #Remove whitespace
     Percentage_data$RNA_species = trimws(Percentage_data$RNA_species, "left")
 
     # Convert from wide to long format
     Percentage_data_long = melt(Percentage_data, id.vars = "RNA_species")
+    Total_count_data = melt(Total_reads, id.vars = "RNA_species")
 
     pdf(out_path, width = 11.7, height = 8.3)
+    
+    print(ggplot(Total_count_data, aes(x=variable, y=value)) +
+    geom_bar(width = 1, stat = "identity") + theme_bw() + 
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), plot.title = element_text(hjust = 0.5)) + 
+    labs(y = "Total counts", x = "Samples"))
+    
     print(ggplot(Percentage_data_long, aes(x=RNA_species, y=value, fill=RNA_species)) +
         geom_bar(width = 1, stat = "identity") + theme_bw() + 
         theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position="top") + 
         labs(y = "Percentage") + facet_wrap( ~ variable))
         
-    print(ggplot(Percentage_data_long, aes(x=variable, y=value, fill=variable)) +
-        geom_bar(width = 1, stat = "identity") + theme_bw() + 
-        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position="top") + 
-        labs(y = "Percentage", x = "Samples") + facet_wrap( ~ RNA_species, scales = "free_y"))
+    for (RNA_species_var in unique(Percentage_data_long$RNA_species)) {
+        temp_df = Percentage_data_long[Percentage_data_long$RNA_species == RNA_species_var, ]
+        print(ggplot(temp_df, aes(x=variable, y=value)) +
+            geom_bar(width = 1, stat = "identity") + theme_bw() + 
+            theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), plot.title = element_text(hjust = 0.5)) + 
+            labs(y = "Percentage", x = "Samples") + ggtitle(RNA_species_var))
+    }
+    
     dev.off()
 }
 
